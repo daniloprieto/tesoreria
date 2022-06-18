37fbd570c441ff745331016ebb1b64774e2a1481
Danilo Prieto <danilo.s.prieto@gmail.com>
2022-06-17 22:14:44 -0300

    ng-thermal-print integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
+import { ThermalPrintModule } from 'ng-thermal-print';
+
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ThermalPrintModule,
     SharedModule
   ],
   providers: [],
--- src/app/tesoreria/components/ingress-tithes-and-offers/ingress-tithes-and-offers.component.ts
+++ src/app/tesoreria/components/ingress-tithes-and-offers/ingress-tithes-and-offers.component.ts
@@ -4,10 +4,15 @@ import { User } from 'src/app/core/models/user.model';
 import { AuthService } from '../../../core/services/auth.service';
 import { TicketBase } from '../../../core/models/ticket.model';
 import { AlertService } from '../../../core/services/alert.service';
-import { PrintService } from '../../../core/services/print.service';
+import { PrintService as miprint } from '../../../core/services/print.service';
 import { Validators, FormBuilder } from '@angular/forms';
 import { TYPE } from '../../../core/services/helpers.service';
 import { Subscription, distinct, tap } from 'rxjs';
+
+//prueba
+import { PrintService, UsbDriver, WebPrintDriver } from 'ng-thermal-print';
+import { PrintDriver } from 'ng-thermal-print/lib/drivers/PrintDriver';
+
 
 
 @Component({
@@ -16,6 +21,13 @@ import { Subscription, distinct, tap } from 'rxjs';
   styleUrls: ['./ingress-tithes-and-offers.component.scss']
 })
 export class IngressTithesAndOffersComponent implements OnInit {
+
+  //prueba
+  status: boolean = false;
+  usbPrintDriver!: UsbDriver;
+  webPrintDriver!: WebPrintDriver;
+  ip: string = '';
+  //prueba end
 
   public user!: User;
 
@@ -31,11 +43,26 @@ export class IngressTithesAndOffersComponent implements OnInit {
 
   constructor(
     private _auth: AuthService,
-    private _ticket: TicketService,
-    private _alert: AlertService,
-    private _print: PrintService,
-    private _fb: FormBuilder
-  ) {}
+    // private _ticket: TicketService,
+    // private _alert: AlertService,
+    // private _print: miprint,
+    private _fb: FormBuilder,
+    //prueba
+    private printService: PrintService
+    //prueba end
+  ) {
+    // prueba
+    this.usbPrintDriver = new UsbDriver();
+    this.printService.isConnected.subscribe(result => {
+        this.status = result;
+        if (result) {
+            console.log('Connected to printer!!!');
+        } else {
+        console.log('Not connected to printer.');
+        }
+    });
+    //prueba end
+  }
 
   ngOnInit(): void {
     this._sub$.push(
@@ -43,103 +70,126 @@ export class IngressTithesAndOffersComponent implements OnInit {
     );
   }
 
-  formatterStrings(s: string): string{
-    return s.charAt(0).toUpperCase() + (s.slice(1)).toLowerCase();
+  //prueba
+  requestUsb() {
+    this.usbPrintDriver.requestUsb().subscribe(result => {
+        this.printService.setDriver(this.usbPrintDriver, 'ESC/POS');
+    });
   }
 
-  setTicket(event: any) {
-    event.preventDefault();
-
-    const form = this.ticketForm;
-
-    let name = this.formatterStrings(form.get('name')?.value);
-    let lastName = this.formatterStrings(form.get('lastName')?.value);
-    let digital = form.get('isDigital')?.value ? 1 : 0;
-    let treasurer = this.user.id;
-
-    if (form.invalid) {
-
-      this._alert.showAlert('Debe ingresar nombre y apellido');
-
-    } else {
-
-      let tickets: TicketBase[] = [];
-
-      let ticket: TicketBase = {
-        name, lastName, digital, treasurer,
-        amount: 0,
-        type: '',
-      };
-
-      if (form.get('tithe')?.value <= 0 && form.get('offering')?.value <= 0) {
-
-        this._alert.showAlert('Debe ingresar al menos un valor');
-
-      } else {
-
-        if (form.get('tithe')?.value > 0) {
-          tickets.push(
-            {
-              ...ticket,
-              amount: form.get('tithe')?.value,
-              type: TYPE.TITHE
-            }
-          )
-        }
-
-        if (form.get('offering')?.value > 0) {
-          tickets.push(
-            {
-              ...ticket,
-              amount: form.get('offering')?.value,
-              type: TYPE.OFFERING
-            }
-          )
-        };
-
-        if(tickets.length > 0) this.saveTicket(tickets);
-      }
-
-
-    }
-
+  connectToWebPrint() {
+      this.webPrintDriver = new WebPrintDriver(this.ip);
+      this.printService.setDriver(this.webPrintDriver, 'WebPRNT');
   }
 
-  saveTicket(tickets: TicketBase[]) {
-
-    let savedTickets: TicketBase[] = [];
-
-    tickets.map((ticket, index) => {
-      this._sub$.push(
-        this._ticket.generateTicket(ticket)
-          .pipe(
-            distinct(),
-            tap({
-              next: ({ id }) => {
-                ticket.id = id;
-                savedTickets.push(ticket);
-                if ((index + 1) === tickets.length) {
-                  if (this._print.print('designTicket', savedTickets)) this.reset();
-                }
-              },
-              error: (error) => {
-                this._alert.showAlert('Error al guardar el Ticket');
-                console.error(error);
-              }
-            })
-          )
-          .subscribe()
-      );
-    })
-
+  print(driver?: PrintDriver) {
+      this.printService.init()
+          .setBold(true)
+          .writeLine('Hello World!')
+          .setBold(false)
+          .feed(4)
+          .cut('full')
+          .flush();
   }
+  //prueba end
 
-  reset() {
-    this.ticketForm.reset();
-  }
+  // formatterStrings(s: string): string{
+  //   return s.charAt(0).toUpperCase() + (s.slice(1)).toLowerCase();
+  // }
 
-  ngOnDestroy(): void {
-    this.reset();
-    this._sub$.map((sub) => sub.unsubscribe());
-  }
+  // setTicket(event: any) {
+  //   event.preventDefault();
+
+  //   const form = this.ticketForm;
+
+  //   let name = this.formatterStrings(form.get('name')?.value);
+  //   let lastName = this.formatterStrings(form.get('lastName')?.value);
+  //   let digital = form.get('isDigital')?.value ? 1 : 0;
+  //   let treasurer = this.user.id;
+
+  //   if (form.invalid) {
+
+  //     this._alert.showAlert('Debe ingresar nombre y apellido');
+
+  //   } else {
+
+  //     let tickets: TicketBase[] = [];
+
+  //     let ticket: TicketBase = {
+  //       name, lastName, digital, treasurer,
+  //       amount: 0,
+  //       type: '',
+  //     };
+
+  //     if (form.get('tithe')?.value <= 0 && form.get('offering')?.value <= 0) {
+
+  //       this._alert.showAlert('Debe ingresar al menos un valor');
+
+  //     } else {
+
+  //       if (form.get('tithe')?.value > 0) {
+  //         tickets.push(
+  //           {
+  //             ...ticket,
+  //             amount: form.get('tithe')?.value,
+  //             type: TYPE.TITHE
+  //           }
+  //         )
+  //       }
+
+  //       if (form.get('offering')?.value > 0) {
+  //         tickets.push(
+  //           {
+  //             ...ticket,
+  //             amount: form.get('offering')?.value,
+  //             type: TYPE.OFFERING
+  //           }
+  //         )
+  //       };
+
+  //       if(tickets.length > 0) this.saveTicket(tickets);
+  //     }
+
+
+  //   }
+
+  // }
+
+  // saveTicket(tickets: TicketBase[]) {
+
+  //   let savedTickets: TicketBase[] = [];
+
+  //   tickets.map((ticket, index) => {
+  //     this._sub$.push(
+  //       this._ticket.generateTicket(ticket)
+  //         .pipe(
+  //           distinct(),
+  //           tap({
+  //             next: ({ id }) => {
+  //               ticket.id = id;
+  //               savedTickets.push(ticket);
+  //               if ((index + 1) === tickets.length) {
+  //                 if (this._print.print('designTicket', savedTickets)) this.reset();
+  //               }
+  //             },
+  //             error: (error) => {
+  //               this._alert.showAlert('Error al guardar el Ticket');
+  //               console.error(error);
+  //             }
+  //           })
+  //         )
+  //         .subscribe()
+  //     );
+  //   })
+
+  // }
+
+  // reset() {
+  //   this.ticketForm.reset();
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.reset();
+  //   this._sub$.map((sub) => sub.unsubscribe());
+  // }
 }
