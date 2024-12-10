182c9875e6fd028bacc470c3d54840b57ef4ff7c
Danilo Prieto <danilo.s.prieto@gmail.com>
2024-12-09 21:44:35 -0300

    [FEAT] Add Docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://www.api.iglesiagilgal.com.ar/',
+  api: 'https://www.api.iglesiagilgal.com.ar/',
   hashCode: 'kd8763g5g9d32f4f6gfdf6h45f5f',
   userEncrypted: 'hfsdglr8523dfy69995ft8dt9d+9s',
   passwordEncrypted:'dsff5f456hgfbh8952gf69tyt8ys',
